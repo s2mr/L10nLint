91a956aa6d439d8be5ac2733048c84163d576b9c
Kazumasa Shimomura <shimomura_kazumasa@cyberagent.co.jp>
2023-02-07 18:13:39 +0900

    Fix

--- Sources/l10nlint/Commands/Lint.swift
+++ Sources/l10nlint/Commands/Lint.swift
@@ -2,6 +2,9 @@ import ArgumentParser
 import Foundation
 import L10nLintFramework
 
+// TODO: Lint command
+// Base, enが同じかどうか
+
 // TODO: Correct command
 // - Baseを編集したらそれぞれにコピー // markerをつけてその中身をコピー
 // - 並び順を自動補正
@@ -18,6 +21,9 @@ extension MainTool {
         @Option(help: "Change report format")
         var reporter: String?
 
+        @Flag(help: "When have lint error, exit with error code")
+        var failOnError: Bool = false
+
         func run() throws {
             let configuration = try Configuration.load(customPath: arguments.config)
             let violations = try LintRunner.run(configuration: configuration)
@@ -26,7 +32,7 @@ extension MainTool {
             if !reportString.isEmpty {
                 queuedPrint(reportString)
             }
-            if violations.map(\.severity).contains(.error) {
+            if failOnError && violations.map(\.severity).contains(.error) {
                 throw ExitCode.failure
             }
         }
